05ae5a284ce6d27580bc2467795780bb3d91b09d
OHASHI Hideya <ohachige@gmail.com>
2010-01-22 20:44:31 +0900

    add test

--- lib/ngramr.rb
+++ lib/ngramr.rb
@@ -74,15 +74,3 @@ class NGramSearcher
   end
 
 end
-
-if __FILE__ == $0
-  y = {}
-  s = NGramSearcher.new(:dir => 'search_index', :size => 3, :min => 1)
-  s.wrap(y)
-  y['foo'] = 'abcd京都def'
-  y['bar'] = 'defghi東京都'
-  y['baz'] = 'bccdde'
-  ['abc', 'def', 'ghi', '京都', '東京', '奈良', 'bcde', '京'].each do |q|
-    puts "#{q} => #{s.search(q).join(', ')} (#{s.search(q, true).join(', ')})"
-  end
-end
--- test/test_ngramr.rb
+++ test/test_ngramr.rb
@@ -1,7 +1,26 @@
 require 'helper'
 
 class TestNgramr < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "" do
+    y = {}
+    s = NGramSearcher.new(:dir => 'search_index', :size => 3, :min => 1)
+    s.wrap(y)
+    y['foo'] = 'abcd京都def'
+    y['bar'] = 'defghi東京都'
+    y['baz'] = 'bccdde'
+    results = ['abc', 'def', 'ghi', '京都', '東京', '奈良', 'bcde',
+        '京'].map do |q|
+      [s.search(q).join(', '),  s.search(q, true).join(', ')]
+    end
+    assert_equal results, [
+        ["foo", "foo"],
+        ["foo, bar", "foo, bar"],
+        ["bar", "bar"],
+        ["foo, bar", "foo, bar"],
+        ["bar", "bar"],
+        ["", ""],
+        ["", "foo, baz"],
+        ["foo, bar", "foo, bar"],
+        ]
   end
 end
